5b99c9ceedd9ff835d66abd633f9338151fefd8e
VClaw <vclaw@example.com>
2026-05-01 10:22:40 +0300

    refactor: ToolOutputHelper requires IStreamShellHost, no raw Console fallbacks

--- src/OpenClawPTT/code/Services/AgentOutput/ToolRenderers/ToolOutputHelper.cs
+++ src/OpenClawPTT/code/Services/AgentOutput/ToolRenderers/ToolOutputHelper.cs
@@ -1,32 +1,27 @@
-using System;
 using System.Linq;
 using Spectre.Console;
 
 namespace OpenClawPTT.Services;
 
 /// <summary>
-/// Console implementation of IToolOutput using System.Console.
-/// When a StreamShell host is provided, routes output through it as markup.
+/// Console implementation of IToolOutput using StreamShell.
+/// Routes all output through the StreamShell host as markup.
 /// </summary>
 public sealed class ToolOutputHelper : IToolOutput
 {
-    private readonly IConsole? _console;
-    private readonly IStreamShellHost? _shellHost;
+    private readonly IStreamShellHost _shellHost;
 
     /// <summary>
     /// Creates a ToolOutputHelper.
     /// </summary>
-    /// <param name="console">Optional IConsole for raw console output.</param>
-    /// <param name="shellHost">Optional StreamShell host for markup output.</param>
-    public ToolOutputHelper(IConsole? console = null, IStreamShellHost? shellHost = null)
+    /// <param name="shellHost">StreamShell host for markup output.</param>
+    public ToolOutputHelper(IStreamShellHost shellHost)
     {
-        _console = console;
         _shellHost = shellHost;
     }
 
     private void WriteToShell(string text, ConsoleColor color)
     {
-        if (_shellHost == null) return;
         var colorName = color switch
         {
             ConsoleColor.Gray => "grey",
@@ -44,39 +39,12 @@ public sealed class ToolOutputHelper : IToolOutput
 
     public void Print(string text, ConsoleColor color = ConsoleColor.White)
     {
-        // For StreamShell, accumulate in a single line
-        if (_shellHost != null)
-        {
-            WriteToShell(text, color);
-        }
-        else if (_console != null)
-        {
-            _console.ForegroundColor = color;
-            _console.Write(text);
-        }
-        else
-        {
-            Console.ForegroundColor = color;
-            Console.Write(text);
-        }
+        WriteToShell(text, color);
     }
 
     public void PrintLine(string text, ConsoleColor color = ConsoleColor.White)
     {
-        if (_shellHost != null)
-        {
-            WriteToShell(text, color);
-        }
-        else if (_console != null)
-        {
-            _console.ForegroundColor = color;
-            _console.WriteLine(text);
-        }
-        else
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(text);
-        }
+        WriteToShell(text, color);
     }
 
     public void PrintTruncated(string text, string continuationPrefix, int rightMarginIndent, ConsoleColor color = ConsoleColor.White)
@@ -85,64 +53,16 @@ public sealed class ToolOutputHelper : IToolOutput
 
         var allLines = text.Split('\n');
         var displayLines = allLines.Take(4).ToArray();
-        var displayContent = string.Join("\n", displayLines);
         bool hasMore = allLines.Length > 4;
 
-        if (_shellHost != null)
+        foreach (var line in displayLines)
         {
-            // Write each line separately to StreamShell
-            foreach (var line in displayLines)
-            {
-                if (!string.IsNullOrWhiteSpace(line))
-                    WriteToShell(line, color);
-            }
-            if (hasMore)
-                WriteToShell($"... ({allLines.Length - 4} more lines)", ConsoleColor.DarkGray);
+            if (!string.IsNullOrWhiteSpace(line))
+                WriteToShell(line, color);
         }
-        else
-        {
-            if (_console != null)
-                _console.ForegroundColor = color;
-            else
-                Console.ForegroundColor = color;
-
-            var consoleWidth = _console?.WindowWidth ?? GetConsoleWidth();
-            var formatter = new AgentReplyFormatter(continuationPrefix, rightMarginIndent, prefixAlreadyPrinted: true, consoleWidth, _console);
-            formatter.ProcessDelta(displayContent);
-            formatter.Finish();
-
-            if (_console != null)
-                _console.ResetColor();
-            else
-                Console.ResetColor();
-
-            if (hasMore)
-            {
-                if (_console != null)
-                {
-                    _console.ForegroundColor = ConsoleColor.DarkGray;
-                    _console.Write($"  ... ({allLines.Length - 4} more lines)");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write($"  ... ({allLines.Length - 4} more lines)");
-                }
-            }
-        }
+        if (hasMore)
+            WriteToShell($"... ({allLines.Length - 4} more lines)", ConsoleColor.DarkGray);
     }
 
-    public void ResetColor()
-    {
-        if (_console != null)
-            _console.ResetColor();
-        else
-            Console.ResetColor();
-    }
-
-    private static int GetConsoleWidth()
-    {
-        try { return Console.WindowWidth; }
-        catch { return 80; }
-    }
+    public void ResetColor() { }
 }
