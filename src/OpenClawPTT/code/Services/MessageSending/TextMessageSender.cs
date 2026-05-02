10f00868fb0bb1afa094083eb87503d8c0d06b26
Venando <eld.venando@gmail.com>
2026-05-02 23:08:53 +0300

    fix(messagesender): fixed double message sending

--- src/OpenClawPTT/code/Services/MessageSending/TextMessageSender.cs
+++ src/OpenClawPTT/code/Services/MessageSending/TextMessageSender.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace OpenClawPTT.Services;
 
 /// <summary>Unified text message sender that sends text directly to the gateway.</summary>
