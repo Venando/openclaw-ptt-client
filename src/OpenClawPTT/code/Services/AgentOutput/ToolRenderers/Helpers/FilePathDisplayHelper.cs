72b452949b1c8488673a675f5541ec269f1ab4d9
Venando <eld.venando@gmail.com>
2026-05-08 22:49:02 +0300

    fix: file path display lenght to 25

--- src/OpenClawPTT/code/Services/AgentOutput/ToolRenderers/Helpers/FilePathDisplayHelper.cs
+++ src/OpenClawPTT/code/Services/AgentOutput/ToolRenderers/Helpers/FilePathDisplayHelper.cs
@@ -12,7 +12,7 @@ internal static class FilePathDisplayHelper
     /// <summary>
     /// The maximum number of characters to display for the full path.
     /// </summary>
-    private const int MaxLength = 120;
+    private const int MaxLength = 25;
 
     /// <summary>
     /// Length of the ".." separator used when truncating.
